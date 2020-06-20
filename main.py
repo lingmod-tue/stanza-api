a47783a4bffa92104a2f263a43e90de8e75bebff
Ramon Ziai <ramon@ziai.eu>
2020-06-20 22:31:00 +0200

    simplification

--- main.py
+++ main.py
@@ -7,30 +7,21 @@ import os
 app = FastAPI()
 
 # this is where our configured pipelines go, one per language
+languages = os.getenv('STANZA_LANGUAGES', "en").split()
 pipelines = {}
-languages = None
-if 'STANZA_LANGUAGES' in os.environ.keys():
-    languages = os.environ['STANZA_LANGUAGES'].split()
-else:
-    # fallback
-    languages = ['en', 'de', 'pt']
 
-def setup_lang(lang: str):
+for lang in languages:
     # download to make sure we have the model
     stanza.download(lang)
     # construct pipeline
     pipelines[lang] = stanza.Pipeline(lang)
-    return f"Successfully set up model for {lang}!"
-
-for lang in languages:
-    setup_lang(lang)
 
 # data to be sent for analysis requests
 class AnalysisRequest(BaseModel):
     text: str
     lang: str
 
-
+# analysis endpoint
 @app.post("/analyze")
 def analyze(req: AnalysisRequest):
     # process and return resulting document as dict
