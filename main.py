23a270a65e4e0dea4d62f7a1dd0c85d698e7f56d
Eric DeMattos <eric.demattos@student.uni-tuebingen.de>
2020-06-22 16:14:56 +0200

    Add support for specifying language packages

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import stanza
 import os
+import sys
 
 # ASGI app variable
 app = FastAPI()
 
 # this is where our configured pipelines go, one per language
 languages = os.getenv('STANZA_LANGUAGES', "en").split()
+packages = os.getenv('STANZA_PACKAGES', 'default').split()
+
+if len(packages) != len(languages):
+    packages = ['default' for _ in languages]
+
 pipelines = {}
 
-for lang in languages:
+for lang, pack in zip(languages, packages):
     # download to make sure we have the model
-    stanza.download(lang)
+    stanza.download(lang, package=pack)
     # construct pipeline
     pipelines[lang] = stanza.Pipeline(lang)
 
